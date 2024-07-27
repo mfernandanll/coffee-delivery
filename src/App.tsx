04863937e7ad576c55b4a2b2796e74262a042887
Fernanda Loureiro <m.fernanda.nll@gmail.com>
2024-07-27 13:35:46 -0400

    Installing and implementing react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 
 import { ThemeProvider } from 'styled-components'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
 
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
-// import { Home } from './pages/Home'
-import { DefaultLayout } from './layouts/DefaultLayout'
+
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <DefaultLayout/>
+      <BrowserRouter>
+        <Router/>
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
