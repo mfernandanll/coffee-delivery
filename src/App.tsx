de0fc439d7813d0089f723847519e57ce1086473
Fernanda Loureiro <m.fernanda.nll@gmail.com>
2024-07-27 13:02:14 -0400

    Adding initial styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import './App.css'
+
+import { ThemeProvider } from 'styled-components'
+
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <h1>Hello World</h1>
-    </>
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
---
+++ src/styles/global.ts
@@ -0,0 +1,38 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  :focus {
+    outline: none;
+    box-shadow: 0 0 0 2px ${(props) => props.theme['yellow-900']};
+  }
+
+  body {
+    background: ${(props) => props.theme['gray-200']};
+    color: ${(props) => props.theme['gray-900']};
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button {
+    font-family: 'Roboto', sans-serif;
+    font-weight: 400;
+    font-size: 1rem;
+  }
+
+  h1, h2, h3 {
+    font-family: "Baloo 2", sans-serif;
+  }
+
+  h1, h2 {
+    font-weight: 800;
+  }
+
+  h3 {
+    font-weight: 700;
+  }
+`
